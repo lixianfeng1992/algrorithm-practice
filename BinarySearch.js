a394a7041b6efc9a1c0372c1eccaf9dcb65cb7ca
Allen Li <allen.li@maiscrm.com>
2019-09-05 10:26:39 +0800

    feat(binarySearch): update
    Signed-off-by: Allen Li <allen.li@maiscrm.com>

--- BinarySearch.js
+++ BinarySearch.js
@@ -24,18 +24,19 @@
 //   return -1;
 // }
 
-function bsearch(array, value) {
-  let low = 0;
-  let high = array.length - 1;
+// function bsearch(array, value) {
+//   let low = 0;
+//   let high = array.length - 1;
 
-  while(low <= high) {
-    let mid = Math.floor((low + high) / 2);
-    if (a[mid] >= value) {
-      if ((mid === 0) || a[mid - 1] < value) return mid;
-      else high = mid - 1;
-    } else {
-      low = mid + 1
-    }
-  }
-  return -1;
-}
+//   while(low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+//     if (a[mid] >= value) {
+//       if ((mid === 0) || a[mid - 1] < value) return mid;
+//       else high = mid - 1;
+//     } else {
+//       low = mid + 1
+//     }
+//   }
+//   return -1;
+// }
+
